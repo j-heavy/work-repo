77a2780481e31a6ddceca44a7d8f2d3f1ec013f2
j-heavy <heavy-ddk@yandex.ru>
2021-07-24 20:58:53 +0300

    publish build

---
+++ build/precache-manifest.5956c6c0a82331d0923caf6a157fdebb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c83f6de6e34c8f7886d9",
+    "url": "/static/css/main.6d732552.chunk.css"
+  },
+  {
+    "revision": "c83f6de6e34c8f7886d9",
+    "url": "/static/js/main.8cf8625b.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "23bbe6b68ed8898a2660",
+    "url": "/static/css/2.b66e9a81.chunk.css"
+  },
+  {
+    "revision": "23bbe6b68ed8898a2660",
+    "url": "/static/js/2.dff9b0cc.chunk.js"
+  },
+  {
+    "revision": "f213b5280c303dc419f8f86ac8d20bc1",
+    "url": "/static/media/Rectangle.f213b528.png"
+  },
+  {
+    "revision": "d2ea67d5c74e0ea13925659752d58f37",
+    "url": "/index.html"
+  }
+];
